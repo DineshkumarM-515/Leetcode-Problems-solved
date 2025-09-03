57a77ca5f43e70ab8c948e5600a62fd2cae76a50
DineshkumarM-515 <dkdkm515@gmail.com>
2025-09-03 10:04:48 +0530

    2942-find-words-containing-character.java

--- easy/2942-find-words-containing-character/2942-find-words-containing-character.java
+++ easy/2942-find-words-containing-character/2942-find-words-containing-character.java
@@ -1,6 +1,16 @@
-            }
-        }
-        
-        return li;
-    }
+class Solution {
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        List<Integer> li = new ArrayList<>();
+        for(int i=0;i<words.length;i++){
+            char[] arr = words[i].toCharArray();
+            for(char c:arr){
+                if(c==x){ 
+                    li.add(i); 
+                    break;
+                }
+            }
+        }
+        
+        return li;
+    }
 }
