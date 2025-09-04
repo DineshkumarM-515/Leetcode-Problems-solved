525ea67b9b0d7cf392d92610a15e8230e34adff4
DineshkumarM-515 <dkdkm515@gmail.com>
2025-09-04 09:31:35 +0530

    Add solution for 3516. Find Closest Person

---
+++ easy/3516-find-closest-person/3516-find-closest-person.java
@@ -0,0 +1,9 @@
+        int p2 = Math.abs(y-z);
+        if(p1 < p2) return 1;
+        if(p1 > p2) return 2;
+        return 0; 
+    }
+}
+        int p1 = Math.abs(x-z);
+    public int findClosest(int x, int y, int z) {
+class Solution {
