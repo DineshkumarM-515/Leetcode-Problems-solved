cd1ac406f354923a277d0c19d2ac381997a40a3c
Dineshkumar M <dkdkm515@gmail.com>
2026-06-16 22:20:46 +0530

    Time: 3 ms (94.36%) | Memory: 48 MB (17.67%) - LeetSync

--- 3462-vowels-game-in-a-string/vowels-game-in-a-string.java
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.java
@@ -1,9 +1,10 @@
 class Solution {
     public boolean doesAliceWin(String s) {
-        String check = "aeiou";
         for(int i=0;i<s.length();i++){
             char ch = s.charAt(i);
-            if(check.indexOf(ch) != -1) return true;
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+                return true;
+            } 
         }
         return false;
     }
