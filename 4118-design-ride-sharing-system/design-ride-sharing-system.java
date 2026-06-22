feff9550cbbef20c1f7fd87cfcc33ffaab8e693f
Dineshkumar M <dkdkm515@gmail.com>
2026-06-22 12:28:06 +0530

    Time: 49 ms (82.29%) | Memory: 48.4 MB (13.62%) - LeetSync

---
+++ 4118-design-ride-sharing-system/design-ride-sharing-system.java
@@ -0,0 +1,36 @@
+class RideSharingSystem {
+    Queue<Integer> Driver;
+    Queue<Integer> Rider;
+    public RideSharingSystem() {
+        Driver = new LinkedList<>();
+        Rider = new LinkedList<>();
+    }
+    
+    public void addRider(int riderId) {
+        Rider.add(riderId);
+    }
+    
+    public void addDriver(int driverId) {
+        Driver.add(driverId);
+    }
+    
+    public int[] matchDriverWithRider() {
+        if(Driver.isEmpty() || Rider.isEmpty()){
+            return new int[]{-1,-1};
+        }
+        else return new int[]{Driver.poll(), Rider.poll()};
+    }
+    
+    public void cancelRider(int riderId) {
+        Rider.remove(Integer.valueOf(riderId));
+    }
+}
+
+/**
+ * Your RideSharingSystem object will be instantiated and called as such:
+ * RideSharingSystem obj = new RideSharingSystem();
+ * obj.addRider(riderId);
+ * obj.addDriver(driverId);
+ * int[] param_3 = obj.matchDriverWithRider();
+ * obj.cancelRider(riderId);
+ */
