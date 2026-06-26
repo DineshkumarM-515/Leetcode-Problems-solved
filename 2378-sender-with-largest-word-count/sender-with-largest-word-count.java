52a5f550a2f7cf9557765b3234767560acd6a955
Dineshkumar M <dkdkm515@gmail.com>
2026-06-26 12:20:20 +0530

    Time: 120 ms (9.71%) | Memory: 58.4 MB (16.99%) - LeetSync

---
+++ 2378-sender-with-largest-word-count/sender-with-largest-word-count.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String largestWordCount(String[] messages, String[] senders) {
+        TreeMap<String, Integer> map = new TreeMap<>();
+        int n = messages.length;
+        for(int i=0;i<n;i++){
+            String[] words = messages[i].split(" ");
+            int len = words.length;
+            if(!map.containsKey(senders[i])){
+                map.putIfAbsent(senders[i] , len);
+            }
+            else{
+                map.put(senders[i], map.get(senders[i])+len);
+            }
+        }
+
+        System.out.println(map);
+
+        String ans = map.firstKey();
+        int cnt = map.get(ans);
+        for(Map.Entry<String,Integer> e : map.entrySet()){
+            String str = e.getKey();
+            int f = e.getValue();
+            if(cnt == f && ans.compareTo(str) < 0){
+                ans = str;
+            }
+            else if(f > cnt){
+                ans = str;
+                cnt = f;
+            }
+        }
+
+        return ans;
+    }
+}
