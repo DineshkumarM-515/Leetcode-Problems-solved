238892885e993c97dda48189ebcfe80ba7bbe581
Dineshkumar M <dkdkm515@gmail.com>
2026-07-08 14:58:43 +0530

    Time: 4 ms (99.45%) | Memory: 109.5 MB (31.87%) - LeetSync

--- 3936-split-array-by-prime-indices/split-array-by-prime-indices.java
+++ 3936-split-array-by-prime-indices/split-array-by-prime-indices.java
@@ -1,25 +1,33 @@
 class Solution {
-    public boolean isPrime(int num) {
-        if (num == 0 || num == 1)
-            return false;
-        for (int i = 2; i <= Math.sqrt(num); i++) {
-            if (num % i == 0)
-                return false;
-        }
-        return true;
-    }
-
     public long splitArray(int[] nums) {
         long primeSum = 0;
         long compositeSum = 0;
 
         int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            if (isPrime(i))
-                primeSum += nums[i];
-            else
-                compositeSum += nums[i];
+        boolean[] sieve = new boolean[n];
+        if (n > 2){
+            Arrays.fill(sieve, true);
+            sieve[0] = false;
+            sieve[1] = false;
+
+            for (int i = 2; i * i < n; i++) {
+                if (sieve[i]) {
+                    for (int j = i * i; j < n; j += i) {
+                        sieve[j] = false;
+                    }
+                }
+            }
         }
+        
+
+        for (int i = 0; i < n; i++) {
+            if (i >= 2 && sieve[i]) {
+                primeSum += nums[i];
+            } else {
+                compositeSum += nums[i];
+            }
+        }
+        
 
         long ans = Math.abs(primeSum - compositeSum);
         return ans;
